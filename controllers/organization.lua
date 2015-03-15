1ecb466fa86952e066037dde977e827d68a8514e
Etiene Dalcol <dalcol@etiene.net>
2015-03-15 05:06:53 +0000

    get organization list

--- controllers/organization.lua
+++ controllers/organization.lua
@@ -1,4 +1,15 @@
 local M = {}
+local cjson = require "cjson"
+
+local function unfunction(obj)
+	for k,v in pairs(obj) do 
+		if type(v) == 'function' then
+			obj[k] = nil
+		elseif type(v) == 'table' then
+			unfunction(v)
+		end
+	end
+end
 
 function M.index(page)
 	local organizations = sailor.model("organization"):find_all()
@@ -40,6 +51,16 @@ function M.view(page)
 		return 404
 	end
 	page:render('view',{organization = organization})
+end
+
+
+function M.getList(page)
+	page.theme = nil
+	local date = os.date("%Y-%m-%d %H:%M:%S")
+	local draw = sailor.model('draw'):find("date_begin <= '" .. date .. "' AND date_end >= '" .. date .. "'")
+	local orgs = draw.organizations
+	unfunction(orgs)
+	page:write(cjson.encode(orgs))
 end
 
 function M.delete(page)
